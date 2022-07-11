6f9e33469eebf51ffc23ff6740d074906164f9ee
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-11 22:12:05 +0300

    exceptions +1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -16,6 +16,15 @@ namespace Exception
                 Console.WriteLine("Введите число:");
                 string? str = Console.ReadLine();
                 int num = Convert.ToInt32(str);
+                // if (str != "" && str != null)
+                // {
+                //     //какой то код
+                // }
+                // // эта штука не пригодиться. один символ, он проверяет все, вне зависимотси тру уже или фолз 
+                // if (str != "" & str != null)
+                // {
+                //     //какой то код
+                // }
 
             }
             catch 
@@ -23,6 +32,11 @@ namespace Exception
                 
                 Console.WriteLine("Вы ввели неправильные данные");
                 Main();
+            }
+            finally
+            {
+                // будет просто 
+                Console.WriteLine("finally");
             }
         }
     }
