d4883ca2e1b2a5c3ad96115643ed077c195fd058
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-11 21:11:06 +0300

    exceptions

---
+++ Example1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+namespace Exception
+{
+    class TryCatch
+    {
+        static void Main()
+        {
+            // string? str = Console.ReadLine();
+            // int num = Convert.ToInt32(str);
+
+            try
+            {
+                Console.WriteLine("Введите число:");
+                string? str = Console.ReadLine();
+                int num = Convert.ToInt32(str);
+
+            }
+            catch 
+            {
+                
+                Console.WriteLine("Вы ввели неправильные данные");
+                Main();
+            }
+        }
+    }
+}
